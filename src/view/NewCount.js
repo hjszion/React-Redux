beac2491a31064a48971ed3b24014f3326a2c386
zionHuang <hjszion@gmail.com>
2019-10-26 01:35:38 -0700

    update

--- src/view/NewCount.js
+++ src/view/NewCount.js
@@ -4,15 +4,17 @@ import { connect } from 'react-redux';
 import { NumActionCreators } from '../actions/NumAction';
 
 // 把容器组件的状态映射到UI组件的props里面去。  state是redux里面的状态数据。
+// UI组件的输入逻辑
 function mapStateToProps(state) {
   return {
-    WebSite: 'aicoder.com',
+    WebSite: 'google.com',
     Num: state.Num
   };
 }
 
 // dispatch 是redux中的分发action的api函数。
 // 事件逻辑都是放在这里，映射给UI组件的方法。
+// UI组件的输出逻辑
 function mapDispatchToProps(dispatch) {
   return {
     addNum(num) {
@@ -24,7 +26,7 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-// UI 组件负责展示逻辑和事件绑定
+// UI 组件负责展示逻辑和事件绑定 这里NewCount就是这个UI组件
 class NewCount extends Component {
   render() {
     return (
