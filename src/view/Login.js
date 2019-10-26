aa67ffbdb112f28755b8becf6f0b14d5af551c74
zionHuang <hjszion@gmail.com>
2019-10-26 00:27:52 -0700

    update

--- src/view/Login.js
+++ src/view/Login.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Login extends Component {
   loginClick = () => {
     //直接把用户校验完的信息，加入到sessionStoryage里去，然后跳转首页
-    sessionStorage.setItem('APP_LOGIN_USER', JSON.stringify({Name: 'aicoder.com', pwd: '123'}));
+    sessionStorage.setItem('APP_LOGIN_USER', JSON.stringify({Name: 'google.com', pwd: '666666'}));   //JSON.stringify() 将对象或者数组转为JSON字符串
     //判断用户之前有没有输入之前的地址。
     let lastLocation = JSON.parse(sessionStorage.getItem('APP_LAST_URL'))
     if(lastLocation) {
